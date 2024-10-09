5368149eb46bc4f40ddb78db9c6d4f9e2bf9c5ae
finteger <nashassociatesinc@gmail.com>
2024-10-09 11:38:14 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -11,6 +11,20 @@
 
 */
 
-/* Display date & time */
 
-document.getElementById("dateNow").innerHTML = `m/d/y/ <br /> h:m:s`;
+
+/* Display the time left until New Years Eve */
+
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
+
+/* Store the current date & time */
+var currentDay = new Date("May, 23, 2018 14:35:05");
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+/* Display date & time */
+document.getElementById("dateNow").innerHTML = 
+dateStr + "<br />" + timeStr;
