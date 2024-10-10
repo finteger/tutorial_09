9552fc6c8f56fb795bf3a6848141db667f08cfb3
finteger <nashassociatesinc@gmail.com>
2024-10-10 10:42:17 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -12,13 +12,6 @@
 */
 
 
-
-/* Display the time left until New Years Eve */
-document.getElementById("days").textContent = "dd";
-document.getElementById("hrs").textContent = "hh";
-document.getElementById("mins").textContent = "mm";
-document.getElementById("secs").textContent = "ss";
-
 /* Store the current date & time */
 var currentDay = new Date("October, 10, 2024 14:35:05");
 var dateStr = currentDay.toLocaleDateString();
@@ -30,3 +23,14 @@ dateStr + "<br />" + timeStr;
 
 /* Display days left before January 1st, 2018 */
 var newYear = new Date("January 1, 2024");
+var nextYear = currentDay.getFullYear() + 1;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
+
+document.getElementById("dateNow").innerHTML = "m/d/y <br /> h:m:s";
+
+/* Display the time left until New Years Eve */
+document.getElementById("days").textContent = Math.floor(daysLeft);
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
